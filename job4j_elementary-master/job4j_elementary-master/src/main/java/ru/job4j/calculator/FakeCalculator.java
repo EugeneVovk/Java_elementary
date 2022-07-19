918257f2229cb45df22d97405992639bfe4a8e84
Eugene Vovk <eugenevovk@gmail.com>
2022-07-19 12:15:46 +0300

    add methods in FakeCalculator

--- job4j_elementary-master/job4j_elementary-master/src/main/java/ru/job4j/calculator/FakeCalculator.java
+++ job4j_elementary-master/job4j_elementary-master/src/main/java/ru/job4j/calculator/FakeCalculator.java
@@ -7,24 +7,38 @@ public class FakeCalculator {
         int six = 6;
         int four = 4;
         int five = 5;
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
+        FakeCalculator.plus(one, two);
+        FakeCalculator.div(six, two);
+        FakeCalculator.minus(five, two);
+        FakeCalculator.time(four, two);
         int age = 18;
-        age = age + 10;
+        FakeCalculator.plus(age, 10);
         int size = 10;
         size = 100;
-        size -= five;
-        int result = one + two;
-        System.out.println(result);
+        FakeCalculator.minus(size, 5);
         one = 10;
         two = 11;
-        result = one + two;
-        int[] array = {onePlusTwo, sixDivTwo, fiveMinusTwo, fourTimeTwo, age, size, result};
+        FakeCalculator.plus(one, two);
 
-        for (int action : array) {
-            System.out.println(action);
-        }
+    }
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void div(int first, int second) {
+        int result = first / second;
+        System.out.println(result);
+    }
+
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
+    public static void time(int first, int second) {
+        int result = first * second;
+        System.out.println(result);
     }
 }
