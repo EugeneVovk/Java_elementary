ce5f634d9c9d7f42ae6500fba9a35dae470e8a9d
Eugene Vovk <eugenevovk@gmail.com>
2022-07-19 13:53:47 +0300

    upd TrgArea

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -1,15 +1,15 @@
 package ru.job4j.condition;
 
 /**
- *  написать формулу вычисления площади треугольника.
+ * Написать формулу вычисления площади треугольника.
  * Площадь треугольника вычисляется по формуле Герона.
  * где a, b и с - это длины сторон, а P - это полупериметр.
  * Полупериметр вычисляется как сумма всех сторон, деленная на 2.
  */
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double rsl = -1;
-        return rsl;
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
