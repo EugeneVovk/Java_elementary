3c61281af996a475d94e7c7a59f95b4d87641ba4
Eugene Vovk <eugenevovk@gmail.com>
2022-07-19 16:23:26 +0300

    add Types

---
+++ src/main/java/ru/job4j/Types.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+public class Types {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = b;
+        int i = s;
+        long l = i;
+        System.out.println("Вывод в консоль значения после расширения: " + l);
+
+        int intNum = 2_147_483_600;
+        float f = intNum;
+        System.out.println(f);
+
+        int intNum2 = 10000;
+        byte byteNum = (byte) intNum2;
+        short shortNum = (short) intNum2;
+        double d = 94.984751;
+        int intNum3 = (int) d;
+
+        System.out.println("Вывод в консоль значения после преобразования: " + byteNum);
+        System.out.println("Вывод в консоль значения после преобразования: " + shortNum);
+        System.out.println("Вывод в консоль значения после преобразования: " + intNum3);
+
+        int intNum4 = 100;
+        char c = (char) intNum4;
+        System.out.println("Вывод в консоль значения после преобразования: " + c);
+    }
+}
