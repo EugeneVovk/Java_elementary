365a1ce8196faf2e189b8b7119789abc347da860
Eugene Vovk <eugenevovk@gmail.com>
2022-07-19 12:00:32 +0300

    upd lesson about variables

--- job4j_elementary-master/job4j_elementary-master/src/main/java/ru/job4j/calculator/FakeCalculator.java
+++ job4j_elementary-master/job4j_elementary-master/src/main/java/ru/job4j/calculator/FakeCalculator.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.calculator;
 
 public class FakeCalculator {
     public static void main(String[] args) {
@@ -11,7 +11,17 @@ public class FakeCalculator {
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
-        int[] array = {onePlusTwo, sixDivTwo, fiveMinusTwo, fourTimeTwo};
+        int age = 18;
+        age = age + 10;
+        int size = 10;
+        size = 100;
+        size -= five;
+        int result = one + two;
+        System.out.println(result);
+        one = 10;
+        two = 11;
+        result = one + two;
+        int[] array = {onePlusTwo, sixDivTwo, fiveMinusTwo, fourTimeTwo, age, size, result};
 
         for (int action : array) {
             System.out.println(action);
