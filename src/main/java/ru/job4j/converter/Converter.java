7cd00fb535c800469382c2dd988d6f74439b6559
Eugene Vovk <eugenevovk@gmail.com>
2022-07-21 17:00:21 +0300

    upd Converter

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -6,17 +6,20 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        return value / 57.15F;
+        return value / 70F;
     }
 
     public static float rubleToDollar(float value) {
-        return value / 56.56F;
+        return value / 60F;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
-        System.out.printf("140 rubles are %.2f euro.\n", euro);
-        System.out.printf("140 rubles are %.2f dollars.\n", dollar);
+        float inEuro = 140;
+        float inDollars = 120;
+        float expected = 2;
+        float actualEuro = Converter.rubleToEuro(inEuro);
+        float actualDollars = Converter.rubleToDollar(inDollars);
+        System.out.println("140 rubles are 2 euro. Test result : " + (expected == actualEuro));
+        System.out.println("140 rubles are 2 dollars. Test result : " + (expected == actualDollars));
     }
 }
