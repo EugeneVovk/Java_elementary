2f142331e891c301d581a75052b5ef8528cf355e
Eugene Vovk <eugenevovk@gmail.com>
2022-07-22 22:17:32 +0300

    add Remain.modulo()

---
+++ src/main/java/ru/job4j/loop/Remain.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Remain {
+    public static int modulo(int number, int divider) {
+        while(number >= divider) {
+            number -= divider;
+        }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат вычисления остатка от деления: " + modulo(21, 16));
+        System.out.println("Результат вычисления остатка от деления: " + modulo(9, 12));
+        System.out.println("Результат вычисления остатка от деления: " + modulo(7, 3));
+        System.out.println("Результат вычисления остатка от деления: " + modulo(15, 5));
+        System.out.println("Результат вычисления остатка от деления: " + modulo(17, 3));
+    }
+}
