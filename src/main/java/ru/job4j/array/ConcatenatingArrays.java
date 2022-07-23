8d1987c111a649dcd5970fe16b5d4f5585f31f93
Eugene Vovk <eugenevovk@gmail.com>
2022-07-23 14:50:22 +0300

    upd ConcatenatingArrays

--- src/main/java/ru/job4j/array/ConcatenatingArrays.java
+++ src/main/java/ru/job4j/array/ConcatenatingArrays.java
@@ -1,5 +1,20 @@
 package ru.job4j.array;
 
+/**
+ * Идея состоит в том, чтобы создать массив,
+ * длина которого равна сумме двух массивов для объединения.
+ * После этого мы должны добавить элементы первого,
+ * а затем элементы второго.
+ * Мы можем использовать метод Arrays.setAll,
+ * чтобы избежать написания цикла
+ * Arrays.setAll(rslArray, i ->
+ * (i < array1.length ? array1[i] : array2[i - array1.length]));
+ * Третий вариант объединения с массивами: System.arraycopy:
+ * System.arraycopy(anArray, 0, resultArray, 0, anArray.length);
+ * System.arraycopy(anotherArray, 0, resultArray, anArray.length, anotherArray.length);
+ * Как мы можем видеть, мы копируем первый массив,
+ * затем второй (после последнего элемента первого).
+ */
 public class ConcatenatingArrays {
     public static void main(String[] args) {
 
