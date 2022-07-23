36ec61b76ca2054f1f9896f417e94196d5de985e
Eugene Vovk <eugenevovk@gmail.com>
2022-07-23 23:29:06 +0300

    add findMin

---
+++ src/main/java/ru/job4j/array/Min.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * Поиск минимального числа в массиве
+ */
+public class Min {
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int el : array) {
+            if (el < min) {
+                min = el;
+            }
+        }
+        return min;
+    }
+}
